c9b3f8148df243dd30497afcba8aefbdd24a9400
Sebastian Pipping <sping@gentoo.org>
2020-02-09 15:14:30 +0100

    euscanwww: Support loading oad Django SECRET_KEY from environment
    Signed-off-by: Sebastian Pipping <sping@gentoo.org>

--- euscanwww/euscanwww/settings.py
+++ euscanwww/euscanwww/settings.py
@@ -115,7 +115,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('EUSCAN_SECRET_KEY', '')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
