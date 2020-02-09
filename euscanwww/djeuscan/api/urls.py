b5a82548c5d36191d6eae86b33d82069de3b5eaa
Sebastian Pipping <sping@gentoo.org>
2020-02-10 00:11:25 +0100

    Stop using django.conf.urls.defaults for Django >=1.6
    Signed-off-by: Sebastian Pipping <sping@gentoo.org>

--- euscanwww/djeuscan/api/urls.py
+++ euscanwww/djeuscan/api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url, patterns
 
 from piston.resource import Resource
 from handlers import RootHandler, StatisticsHandler, HerdsHandler, \
